29896943705713d529aadfce4f8e9df50fe1c6c0
lmt9273-hue <lmt9273@gmail.com>
2026-08-23 16:59:05 +0530

    Update BOT_TOKEN in main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 # --- CONFIGURATION ---
-BOT_TOKEN = "8868364202:AAFhI1n-PJ-vS0x_OWZlpwN0k7m4GSSLRLI"
+BOT_TOKEN = "8868364202:AAHmY3fFncwmpDjDjbwCWzcg-cuq-xCNbAI"
 REQUIRED_CHANNELS = ["@hacklinkpc"]
 
 # ALLOWED OWNER USERNAMES & ID
