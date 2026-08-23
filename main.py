acf3811b285f12f8244310dac96d6167879d5514
lmt9273-hue <lmt9273@gmail.com>
2026-08-23 15:01:16 +0530

    Change BOT_TOKEN to a new value
    Updated the BOT_TOKEN for the application.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask import Flask
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = "8868364202:AAFhI1n-PJ-vS0x_OWZlpwN0k7m4GSSLRLI"
+BOT_TOKEN = "8868364202:AAHT-nhI3bYNv9gHXADiMPBj5V8AgdjnX8U"
 REQUIRED_CHANNELS = ["@hacklinkpc"]
 OWNER_ID = 7125817223
 OWNER_USERNAME = "rohit2848"
